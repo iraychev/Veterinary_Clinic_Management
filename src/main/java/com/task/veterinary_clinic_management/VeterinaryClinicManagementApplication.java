c3232cb18222716e1b2e8dcd208a01ebeaa01213
iraychev <ivoraychev1337@gmail.com>
2023-07-17 11:56:26 +0300

    Initial commit

---
+++ src/main/java/com/task/veterinary_clinic_management/VeterinaryClinicManagementApplication.java
@@ -0,0 +1,13 @@
+package com.task.veterinary_clinic_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinaryClinicManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VeterinaryClinicManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/task/veterinary_clinic_management/VeterinaryClinicManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.veterinary_clinic_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinaryClinicManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
