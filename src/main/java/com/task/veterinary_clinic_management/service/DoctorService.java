c63ec7b21a94bc8911ca782ec9a093e0e84ae830
iraychev <ivoraychev1337@gmail.com>
2023-07-18 18:36:53 +0300

    Rename FinancialRecord to FinancialRecordService; implement DoctorService and FinancialRecordService

--- src/main/java/com/task/veterinary_clinic_management/service/DoctorService.java
+++ src/main/java/com/task/veterinary_clinic_management/service/DoctorService.java
@@ -1,8 +1,41 @@
 package com.task.veterinary_clinic_management.service;
 
+import com.task.veterinary_clinic_management.model.Doctor;
+import com.task.veterinary_clinic_management.repository.DoctorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DoctorService {
+    private final DoctorRepository doctorRepository;
 
+    @Autowired
+    public DoctorService(DoctorRepository doctorRepository) {
+        this.doctorRepository = doctorRepository;
+    }
+
+    public List<Doctor> getAllDoctors() {
+        return doctorRepository.findAll();
+    }
+
+    public Doctor getDoctorById(Long id) {
+        return doctorRepository.getById(id);
+    }
+
+    public Doctor addDoctor(Doctor doctor) {
+        return doctorRepository.save(doctor);
+    }
+
+    public Doctor updateDoctor(Long id, Doctor updatedDoctor) {
+        Doctor existingDoctor = doctorRepository.getById(id);
+        existingDoctor.setName(updatedDoctor.getName());
+        existingDoctor.setSpecialization(updatedDoctor.getSpecialization());
+        return doctorRepository.save(existingDoctor);
+    }
+
+    public void deleteDoctor(Long id) {
+        doctorRepository.deleteById(id);
+    }
 }
--- src/main/java/com/task/veterinary_clinic_management/service/FinancialRecord.java
+++
@@ -1,7 +0,0 @@
-package com.task.veterinary_clinic_management.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class FinancialRecord {
-}
